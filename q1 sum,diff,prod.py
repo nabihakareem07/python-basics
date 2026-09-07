8651935aea6b7b5b5cc558de6ac5b04e949b7500
Nabiha Kareem <nabihakareem07@gmail.com>
2026-09-07 15:37:39 +0530

    Add Python practice programs

---
+++ q1 sum,diff,prod.py
@@ -0,0 +1,6 @@
+a=input("enter the first number")
+b=input("enter second number")
+print("sum=", a + b)
+print("difference=", a - b)
+print("product=", a * b)
+print("quotient=", a / b)
---
+++ q20 vowel or consonent
@@ -0,0 +1,5 @@
+ch = input("Enter a character: ")
+if ch in "aeiouAEIOU":
+    print("Vowel")
+else:
+    print("Consonant")
